d72b10c5098d05f77edac6547c0e730d46e221fe
willmac321 <14141270+willmac321@users.noreply.github.com>
2018-03-27 21:26:17 -0500

    working on webGL

--- rectangleObj.js
+++ rectangleObj.js
@@ -1,25 +1,25 @@
 function RectObj(offSet){
-  this.angle=0;
-  this.h=10;
-  this.offset=offSet+1;
-  rectMode(CENTER);
 
+  this.h=10;
+  this.offset=offSet;
+  rectMode(CENTER);
+  this.angle=0;
     this.show = function(){
       fill(255);
-      if (this.offset%2===0){
-        rect(this.offset*w,0,this.offset*w,this.h);
-      }
-      else{
-        rect(-this.offset*w,0,this.offset*w,this.h);
-      }
+      box(w);
+
+     // if (this.offset%2===0){
+     //    rect(this.offset*w,0,w,this.h);
+     //  }
+     //  else{
+     //    rect(-this.offset*w,0,w,this.h);
+     //  }
 
     };
 
-
     this.update = function(){
-
        this.h = map(sin(this.angle),-1,1,1,100);
-       this.angle+=.1+this.offset/1000;
-
+       this.angle+=.1;
+       rotateX(this.angle);
     };
 }
--- sketch.js
+++ sketch.js
@@ -5,8 +5,7 @@ var canv;
 
 function setup() {
 
-  canv=createCanvas(400,400);
-
+  canv=createCanvas(400,400,WEBGL);
 
   for(let i=0;i<w*width/2;i++){
     r.push(new RectObj(i));
@@ -15,7 +14,8 @@ function setup() {
 
 function draw(){
   background(51);
-translate(width/2,height/2);
+//translate(width/2,height/2);
+   translate(width/2,0,0);
   for(let i=0;i<r.length;i++){
     r[i].update();
     r[i].show();
