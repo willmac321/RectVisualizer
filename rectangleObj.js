58b53c86f1121b1ef63545762da4d2a48ebb9019
willmac321 <14141270+willmac321@users.noreply.github.com>
2018-03-28 17:11:17 -0500

    got a 3d array of boxes that move

--- rectangleObj.js
+++ rectangleObj.js
@@ -1,13 +1,32 @@
 function RectObj(offSet){
-
+  this.rotation= .523599; //0.523599 is 30 deg
   this.h=10;
   this.offset=offSet;
   rectMode(CENTER);
-  this.angle=0;
-    this.show = function(){
-      fill(255);
-      box(w);
+    this.angle=this.offset/.000039;
 
+    this.show = function(){
+      //for (let z=0;z<width/(w);z++){
+
+
+        for(let p=0;p<width/w;p++){
+          for (let i=-1; i<2;i=i+2){
+            if(this.offset===0){
+                this.makeBoxes(0,i*p);
+
+            }
+            else{
+                this.makeBoxes(i,i*p);
+
+                this.makeBoxes(i,p);
+
+                this.makeBoxes(i*i,i*p);
+
+            }
+          }
+        }
+
+  //  }
      // if (this.offset%2===0){
      //    rect(this.offset*w,0,w,this.h);
      //  }
@@ -16,10 +35,31 @@ function RectObj(offSet){
      //  }
 
     };
+    this.makeBoxes=function(i,p){
+      fill(255);
+      push();
+      //rotateZ(this.rotation);
+      rotateX(this.rotation);
+      rotateY(this.rotation);
+//this.offset*w
+      translate(w*this.offset*i,0,p*w);
+      //this.angle+=Math.PI/100;
+
+    this.calcH();
+      box(w,this.h,w);
+      pop();
+    };
 
     this.update = function(){
-       this.h = map(sin(this.angle),-1,1,1,100);
-       this.angle+=.1;
-       rotateX(this.angle);
+
+      this.calcH();
+       this.incAngle();
+    };
+
+    this.calcH=function(){
+      this.h = map(sin(this.angle),-1,1,0,100);
+    };
+    this.incAngle=function(){
+      this.angle+=.1//this.offset*Math.PI*2/180;
     };
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,25 @@
 
-var w=10;
+var w=80;
 var r=[];
 var canv;
+var test;
 
 function setup() {
 
   canv=createCanvas(400,400,WEBGL);
-
-  for(let i=0;i<w*width/2;i++){
+  ortho(-width, width+width/2, height*1.3, -height*1.3, -width*height, width*height*3);
+  //test=new RectObj(0);
+  for(let i=0;i<width/(w);i++){
     r.push(new RectObj(i));
   };
 }
 
 function draw(){
   background(51);
-//translate(width/2,height/2);
-   translate(width/2,0,0);
+  // test.update();
+  // test.show();
+  //translate(width/2,0,0);
+  //
   for(let i=0;i<r.length;i++){
     r[i].update();
     r[i].show();
