15fdedb44aed0bd599b08689ec959e960232afa6
willmac321 <14141270+willmac321@users.noreply.github.com>
2018-04-01 11:04:37 -0500

    Almost working, 45 and 90 deg offsets move inline

--- rectangleObj.js
+++ rectangleObj.js
@@ -1,61 +1,54 @@
-function RectObj(offSet){
+function RectObj(x,y){
   this.rotation= .523599; //0.523599 is 30 deg
-  this.offset=offSet;
-  this.rectCount=this.offset*8;
+  this.offset=Math.max(abs(x),abs(y));
+  this.angle=0;
   rectMode(CENTER);
-  if(this.offset>0){
-    this.ang=2*Math.PI/this.rectCount;
-    this.radius=w*this.offset*sqrt(2);//sqrt(2*pow(w*this.offset,2));
-    this.x=this.radius*(cos(this.ang)/(abs(cos(this.ang))+abs(sin(this.ang))));
-    this.z=this.radius*(sin(this.ang)/(abs(cos(this.ang))+abs(sin(this.ang))));
-    this.d=dist(this.x,this.z,0,0);
-    this.ofs=map(this.x,0,width/2,-1,1);
-    this.angle=this.ofs;
-  }else{
-    this.x=0;
-    this.z=0;
-    this.d=0;
+
+  this.calcAngle=function(x,y){
+    //  this.d=0;
+    this.rectCount=this.offset*8;
     this.ofs=0;
-    this.radius=0;
-    this.angle=0;
+    if(x!=0 || y!=0){
+      this.rectCount=1;
+      this.ang=2*Math.PI/this.rectCount;
+      this.radius=dist(x,y,0,0);
+      this.ofs=(map(this.radius,0,this.offset/2,-1,1));
+    }
+    //*sqrt(2);//sqrt(2*pow(w*this.offset,2));
+    //this.x=this.radius*(cos(this.ang)/(abs(cos(this.ang))+abs(sin(this.ang))));
+    //this.z=this.radius*(sin(this.ang)/(abs(cos(this.ang))+abs(sin(this.ang))));
+    //this.d=dist(this.x,this.z,0,0);
+
+    //this.angle+=this.ofs;
   }
 
+  this.show = function(){
+    this.makeBoxes(x,y);
+  }
 
-    this.show = function(){
+  this.makeBoxes=function(x,y){
+    fill(255);
+    push();
+    rotateX(this.rotation);
+    rotateY(this.rotation);
+    translate(x*w,0,y*w);
+    normalMaterial();
+    box(w,this.h,w);
+    pop();
+  };
 
-      if (this.offset===0){this.makeBoxes(0,0);}
-      for(let x=-this.offset;x<=this.offset;x++){
-        for(let y=-this.offset; y<=this.offset;y++){
-          if(x!=0 || y!=0){
-            this.makeBoxes(x,y);
-          }
-        }
-      }
-    }
+  this.update = function(){
+    this.calcAngle(x,y);
+    this.incAngle();
+    this.calcH();
+  };
 
-    this.makeBoxes=function(x,y){
-      fill(255);
-      push();
-      rotateX(this.rotation);
-      rotateY(this.rotation);
-      translate(x*w,0,y*w);
-      normalMaterial();
-      box(w,this.h,w);
-      pop();
+  this.calcH=function(){
+    this.h = map(sin(this.angle+this.ofs),-1,1,1,100);
+  };
 
-    };
-
-    this.update = function(){
-
-      this.calcH();
-      this.incAngle();
-    };
-
-    this.calcH=function(){
-      this.h = map(sin(this.angle),-1,1,0,100);
-    };
-
-    this.incAngle=function(){
-      this.angle+=radians(5);
-    };
+  this.incAngle=function(){
+    //this.calcAngle();
+    this.angle+=radians(5);
+  };
 }
--- sketch.js
+++ sketch.js
@@ -3,17 +3,29 @@ var w=80;
 var r=[];
 var canv;
 var test;
-
+var res;
 function setup() {
   canv=createCanvas(400,400,WEBGL);
-  ortho(-width, width, height, -height, -width*height, width*height);
+  ortho(-width*1.5, width*1.5, height*1.5, -height*1.5, -width*height, width*height);
   //test=new RectObj(0);
-  r.push(new RectObj(0));
-  for(let i=0;i<width/(w);i++){
-    if(i!=0 ){
-      r.push(new RectObj(i));
+//  r.push(new RectObj(0));
+  //for(let i=0;i<width/(w*4);i++){
+    //if(i!=0 ){
+    res=floor(width/(w*1.5));
+    for(let x=-res;x<=res;x++){
+      for(let y=-res; y<=res;y++){
+
+        //if(x!=0 || y!=0){
+          r.push(new RectObj(x,y));
+        //}
+        //else if(x===0 && y===0){
+          //r.push(new RectObj(0,0));
+        //}
+      }
     }
-  }
+
+    //}
+  //}
 }
 
 function draw(){
@@ -22,7 +34,7 @@ function draw(){
   // test.show();
   //translate(width/2,0,0);
   for(let i=0;i<r.length;i++){
-    r[i].update();
-    r[i].show();
+      r[i].update();
+      r[i].show();
   }
 }
